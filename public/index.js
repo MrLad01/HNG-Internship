e7af091db275e03c6b0c770fdfa6ee0559afd6e4
MrLad01 <ladayodeji@gmail.com>
2023-09-09 15:48:35 +0100

    update

--- public/index.js
+++ public/index.js
@@ -5,22 +5,24 @@ function currentTime() {
     var hours = date.getUTCHours();
     var minutes = date.getUTCMinutes();
     var seconds = date.getUTCSeconds();
-    var paddedSeconds = String(seconds).padStart(2, "0");
-    var paddedMinutes = String(minutes).padStart(2, "0");
+    // var paddedSeconds = String(seconds).padStart(2, "0");
+    // var paddedMinutes = String(minutes).padStart(2, "0");
     
-    var ap = " ";
-    if(hours < 12){
-        ap = "AM"
-    } else { 
-        ap = "PM";
-        hours = hours - 12;
-        var paddedHours = String(hours).padStart(2, "0");
-    }
+    // var ap = " ";
+    // if(hours < 12){
+    //     ap = "AM"
+    // } else { 
+    //     ap = "PM";
+    //     hours = hours - 12;
+    //     var paddedHours = String(hours).padStart(2, "0");
+    // }
 
-    var time = paddedHours + ":" + paddedMinutes + ":" + paddedSeconds + " " + ap;
+    // var time = paddedHours + ":" + paddedMinutes + ":" + paddedSeconds + " " + ap;
+
+    var milliseconds = (hours * 3600 + minutes * 60 + seconds) * 1000;
 
 
-    document.getElementById("currentUTCtime").innerHTML = time;
+    document.getElementById("currentUTCtime").innerHTML = milliseconds;
     document.getElementById("dayoftheweek").innerHTML = daysOfTheWeek[day];
 }
 
