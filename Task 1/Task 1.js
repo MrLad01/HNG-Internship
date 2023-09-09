9dd2cd1b754a75691b1d90e7492ffd5af9552e3c
MrLad01 <ladayodeji@gmail.com>
2023-09-09 06:57:37 +0100

    update

---
+++ Task 1/Task 1.js
@@ -0,0 +1,26 @@
+function currentTime() {
+    var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    var date = new Date();
+    var day = date.getDay();
+    var hours = date.getUTCHours();
+    var minutes = date.getUTCMinutes();
+    var seconds = date.getUTCSeconds();
+    var paddedSeconds = String(seconds).padStart(2, "0");
+    var paddedMinutes = String(minutes).padStart(2, "0");
+    var paddedHours = String(hours).padStart(2, "0");
+
+    var ap = " ";
+    if(hours < 12){
+        ap = "AM"
+    } else { 
+        ap = "PM";
+        hours = hours - 12;
+    }
+
+    var time = paddedHours + ":" + paddedMinutesinutes + ":" + paddedSeconds + " " + ap;
+
+    document.getElementById("currentUTCtime").innerHTML = time;
+    document.getElementById("dayoftheweek").innerHTML = daysOfTheWeek[day];
+}
+
+setInterval(currentTime, 1000);
